277b71e9ac80790b242295cad40f0d1c7fffd13b
TBleess <todd.bleess@state.co.us>
2023-03-17 10:50:25 -0600

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -13,7 +13,7 @@ var h = 730;
 var root = [];
 
 
-var yearLabels = [{	label : '1990',	x : '1'}, {	label : '1991',	x : '2'}, {	label : '1992',	x : '3'}, {	label : '1993',	x : '4'}, {	label : '1994',	x : '5'}, {	label : '1995',	x : '6'}, {	label : '1996',	x : '7'}, {	label : '1997',	x : '8'}, {	label : '1998',	x : '9'}, {	label : '1999',	x : '10'}, {	label : '2000',	x : '11'}, {	label : '2001',	x : '12'}, {	label : '2002',	x : '13'}, {	label : '2003',	x : '14'}, {	label : '2004',	x : '15'}, {	label : '2005',	x : '16'}, {	label : '2006',	x : '17'}, {	label : '2007',	x : '18'}, {	label : '2008',	x : '19'}, {	label : '2009',	x : '20'}, {	label : '2010',	x : '21'}, {	label : '2011',	x : '22'}, {	label : '2012',	x : '23'}, {	label : '2013',	x : '24'}, {	label : '2014',	x : '25'}, {	label : '2015',	x : '26'}, {	label : '2016',	x : '27'}, {	label : '2017',	x : '28'}, {	label : '2018',	x : '29'}, {	label : '2019',	x : '30'}, {	label : '2020',	x : '31'} , {	label : '2021',	x : '32'} , {	label : '2022',	x : '33'}];
+var yearLabels = [{	label : '1990',	x : '1'}, {	label : '1991',	x : '2'}, {	label : '1992',	x : '3'}, {	label : '1993',	x : '4'}, {	label : '1994',	x : '5'}, {	label : '1995',	x : '6'}, {	label : '1996',	x : '7'}, {	label : '1997',	x : '8'}, {	label : '1998',	x : '9'}, {	label : '1999',	x : '10'}, {	label : '2000',	x : '11'}, {	label : '2001',	x : '12'}, {	label : '2002',	x : '13'}, {	label : '2003',	x : '14'}, {	label : '2004',	x : '15'}, {	label : '2005',	x : '16'}, {	label : '2006',	x : '17'}, {	label : '2007',	x : '18'}, {	label : '2008',	x : '19'}, {	label : '2009',	x : '20'}, {	label : '2010',	x : '21'}, {	label : '2011',	x : '22'}, {	label : '2012',	x : '23'}, {	label : '2013',	x : '24'}, {	label : '2014',	x : '25'}, {	label : '2015',	x : '26'}, {	label : '2016',	x : '27'}, {	label : '2017',	x : '28'}, {	label : '2018',	x : '29'}, {	label : '2019',	x : '30'}, {	label : '2020',	x : '31'} , {	label : '2021',	x : '32'} , {	label : '2022',	x : '33'}, {	label : '2023',	x : '34'}];
 
 var monthLabels = [{	label : 'January',	x : '1'}, {	label : 'February',	x : '2'}, {	label : 'March',	x : '3'}, {	label : 'April',	x : '4'}, {	label : 'May',	x : '5'}, {	label : 'June',	x : '6'}, {	label : 'July',	x : '7'}, {	label : 'August',	x : '8'}, {	label : 'September',	x : '9'}, {	label : 'October',	x : '10'}, {	label : 'November',	x : '11'}, {	label : 'December',	x : '12'}, {	label : 'Annual',	x : '14'}];
 
@@ -206,6 +206,7 @@ function calcy(d) {
 	if (year == "2020"){ypos = 600;	};
 	if (year == "2021"){ypos = 620;	};
 	if (year == "2022"){ypos = 640;	};
+	if (year == "2023"){ypos = 660;	};
   
 	return ypos + topmargin;
 
